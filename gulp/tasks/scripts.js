017afcab4e88272516f8a6ef41166f84512f46a6
MironovS <stasmironovweb@gmail.com>
2021-03-22 14:18:03 +0300

    fix slider and bildboard and quote module

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,9 +1,10 @@
 module.exports = function () {
 	var babel = require("gulp-babel"),
-		minify = require('gulp-minify');
+		minify = require('gulp-minify'),
+		concat = require('gulp-concat');
 
 	$.gulp.task('scripts:lib', function () {
-		return $.gulp.src(['node_modules/jquery/dist/jquery.min.js', 'node_modules/svgxuse/svgxuse.js', 'node_modules/object-fit-polyfill/dist/object-fit-polyfill.js', 'node_modules/babel-polyfill/dist/polyfill.js', 'node_modules/swiper/swiper-bundle.min.js', 'node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js', 'node_modules/lightgallery.js/dist/js/lightgallery.min.js', 'node_modules/eventemitter3/umd/eventemitter3.min.js'])
+		return $.gulp.src(['node_modules/jquery/dist/jquery.min.js', 'node_modules/svgxuse/svgxuse.js', 'node_modules/object-fit-polyfill/dist/object-fit-polyfill.js', 'node_modules/babel-polyfill/dist/polyfill.js', 'node_modules/swiper/js/swiper.min.js', 'node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js', 'node_modules/lightgallery.js/dist/js/lightgallery.min.js', 'node_modules/eventemitter3/umd/eventemitter3.min.js'])
 			.pipe($.gp.concat('libs.js'))
 			.pipe(minify())
 			.pipe($.gulp.dest('build/js/'))
@@ -13,7 +14,8 @@ module.exports = function () {
 	});
 
 	$.gulp.task('scripts', function () {
-		return $.gulp.src(['src/dev/js/main.js'])
+		return $.gulp.src(['src/dev/js/**/*.js'])
+			.pipe(concat('main.js'))
 			.pipe(babel())
 			.pipe(minify())
 			.pipe($.gulp.dest('build/js/'))
